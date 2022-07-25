49cc2f3b437765a1d92ccfeec86174d95f9561ce
hevedrios <exxed.ridel@gmail.com>
2022-07-25 15:57:52 -0500

    Webpack, Babel, Sass added

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/App."; //why this works? xd
+
+const root = ReactDOM.createRoot(
+   document.getElementById("root")
+);
+
+root.render(<App />);
