0fc0a0f27865c7c068e5139d2a1ab56458f6fb6a
hevedrios <exxed.ridel@gmail.com>
2022-07-25 21:57:43 -0500

    Template created, added 'Die' component and random dice array functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,25 @@
 import React from "react";
 import "../styles/global.scss";
+import Die from "./Die.";
 
 const App = () => {
+  const [dice, setDice] = React.useState(allNewDice());
+
+  function allNewDice() {
+    const newDice = [];
+    for (let i = 0; i < 10; i++) {
+      newDice.push(Math.ceil(Math.random() * 6));
+      // newDice.push(Math.floor(Math.random() * (7 - 1) + 1));
+    }
+    return newDice;
+  }
+
+  const diceElements = dice.map((die) => <Die value={die} />);
+
   return (
-    <div>
-      <h1>This is gonna be the Markdown notes SPA</h1>
-    </div>
+    <main>
+      <div className="dice-container">{diceElements}</div>
+    </main>
   );
 };
 
---
+++ src/components/Die.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Die = (props) => {
+
+  return (
+    <div className="die-face">{props.value}</div>
+  )
+}
+
+export default Die
