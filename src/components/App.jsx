2192c712101b38264f1659c63d0ccfc9b5255a13
hevedrios <exxed.ridel@gmail.com>
2022-07-26 21:51:07 -0500

    Corrected Roll button so colored dices don't change

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "../styles/global.scss";
 import Die from "./Die.";
+import { nanoid } from "nanoid";
 
 const App = () => {
   const [dice, setDice] = React.useState(allNewDice());
@@ -8,22 +9,51 @@ const App = () => {
   function allNewDice() {
     const newDice = [];
     for (let i = 0; i < 10; i++) {
-      newDice.push(Math.ceil(Math.random() * 6));
-      // newDice.push(Math.floor(Math.random() * (7 - 1) + 1));
+      newDice.push({
+        id: nanoid(),
+        value: Math.ceil(Math.random() * 6), // Math.floor(Math.random() * (7 - 1) + 1);
+        isHeld: false,
+      });
     }
+    console.log(newDice);
     return newDice;
   }
 
-  function rollDice() {
-    setDice(allNewDice());
+  function rollDice(id) {
+    setDice((prevState) =>
+      prevState.map((die) => {
+        return die.isHeld
+          ? die
+          : {
+              ...die,
+              id: nanoid(),
+              value: Math.ceil(Math.random() * 6),
+            };
+      })
+    );
   }
 
-  const diceElements = dice.map((die) => <Die value={die} />);
+  function holdDice(id) {
+    setDice((prevDice) => {
+      return prevDice.map((die) => {
+        return id === die.id ? { ...die, isHeld: !die.isHeld } : die;
+      });
+    });
+  }
+
+  const diceElements = dice.map((die) => (
+    <Die
+      key={die.id}
+      value={die.value}
+      isHeld={die.isHeld}
+      handleClick={() => holdDice(die.id)}
+    />
+  ));
 
   return (
     <main>
       <div className="dice-container">{diceElements}</div>
-      <button className="roll-dice" onClick={rollDice}>
+      <button className="roll-dice" onClick={() => rollDice(dice.id)}>
         Roll
       </button>
     </main>
--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import React from "react";
 
 const Die = (props) => {
+  const styles = {
+    backgroundColor: props.isHeld ? "#59E391" : "white",
+  };
 
   return (
-    <div className="die-face">{props.value}</div>
-  )
-}
+    <div className="die-face" style={styles} onClick={props.handleClick}>
+      {props.value}
+    </div>
+  );
+};
 
-export default Die
+export default Die;
